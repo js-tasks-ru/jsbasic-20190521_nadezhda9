e55cdc4b4f299a36e082903201c0508141f050ac
Matveeva Nadezhda <shonchip@mail.ru>
2019-06-15 19:24:14 +0300

    Added task

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -12,13 +12,14 @@ function getMinMax(string) {
 	string = inputData.replace(/[^-0-9./\s/]/g, ',');
 	
 	let arr = string.split(',');
-
+		
 	let min = Math.min(...arr),
 		max = Math.max(...arr);
-	
+		
 	let res = { min: min,
-				max: max};	
-		return(res);
+				max: max
+	};
+	return res;		
 }
 getMinMax(inputData);
 
