27b87164a97131a93ee37c784543d6a3a5b8cf19
Matveeva Nadezhda <shonchip@mail.ru>
2019-06-15 17:55:04 +0300

    Added task

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -5,7 +5,16 @@
  * @param   {string} str -  входные данные
  * @returns {{min:number, max:number}}  объект
  */
-function getMinMax(str) {
 
+let inputData = '1, -5.8 или 10, хотя 34 + -5.3 и 73';
+
+function getMinMax(str) {
+	let string = inputData.replace(/[^-0-9./\s/]/g, '').replace(/\s{1,}/g, ',');
+	let arr = string.split(',');
+
+	alert('min = ' + Math.min(...arr));
+	alert('max = ' + Math.max(...arr));
 }
+getMinMax();
+
 
