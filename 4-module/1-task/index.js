7642953bf561a3d691143b268ac05c7959a8bc14
Matveeva Nadezhda <shonchip@mail.ru>
2019-06-16 02:20:23 +0300

    Added task

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -6,6 +6,21 @@
  * @return {HTMLUListElement}
  */
 
+let friends = [
+    {
+        firstName: 'Artsiom',
+        lastName: 'Mezin'
+    },
+    {
+        firstName: 'Ilia',
+        lastName: 'Kantor'
+    },
+    {
+        firstName: 'Christopher',
+        lastName: 'Michael'
+    }
+];
+
 function makeFriendsList(friends) {  
 	let ul = document.createElement('ul');
 	document.body.appendChild(ul);
