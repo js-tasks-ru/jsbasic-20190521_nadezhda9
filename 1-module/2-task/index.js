b314fddabdeeda0ecb90b299c080564cce52577e
Matveeva Nadezhda <shonchip@mail.ru>
2019-06-15 14:11:15 +0300

    Added tasks 1-module

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -12,15 +12,14 @@ function print (text) {
  * чтобы функция sayHello работала корректно
  */
 
-function sayHello (name) {
+function sayHello () {
 	let userName = prompt('Введите ваше имя', '');
 
 	function isValid (name) {
-		if (name !== null && name !== '' && name.length > 4 && (!/\s/.test(name))) {
-	        return true;
-		} else {
-			return false;
-		}	
+		if (name === null || name != '' || name.length < 4 || (/\s/.test(name)) {
+	        return false;
+		} 
+		return true;
 	}
 
     if (isValid(userName)) {
@@ -30,4 +29,3 @@ function sayHello (name) {
     }
 }
 sayHello();
-
